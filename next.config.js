6f506ff6c84bcaea7bd73a1d50b47959adefedf2
Naveen-neevaN <naveennagendiran321@gmail.com>
2025-11-08 23:36:09 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -4,13 +4,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Update this to match your GitHub repository name
-  // For example, if your repo is github.com/username/my-website
-  // then basePath should be '/my-website'
+  // Updated to match your GitHub repository name
   basePath: process.env.NODE_ENV === 'production' ? '/Naveen-Profile' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Naveen-Profile' : '',
   trailingSlash: true,
 }
 
 module.exports = nextConfig
+
 
